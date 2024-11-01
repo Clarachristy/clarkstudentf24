6967d02bc94afe3d620436c78c5ffac213a66650
Clarachristy <chi22018@byui.edu>
2024-11-01 13:40:08 -0600

    Update w06_hw.sql

--- code/week6/w06_hw.sql
+++ code/week6/w06_hw.sql
@@ -3,23 +3,40 @@
 
 -- ------------------------------------------------------------------------------------
 
-USE film;
+USE film
 
 -- ------------------------------------------------------------------------------------
 -- 1. Insert into the studio table - Parent Table
-
+INSERT INTO studio 
+(studio_name)
 
 -- ------------------------------------------------------------------------------------
 -- 2. Insert into the rating table - Parent Table
-
+INSERT INTO rating
+(rating_level)
+VALUES
+( 'G' )
+, ( 'PG' )
+, ( 'PG-13' )
 
 -- ------------------------------------------------------------------------------------
 -- 3. Insert into the movie table - Child Table
-
+INSERT INTO movie
+( movie_title
+, movie_year
+, studio_id
+, rating_id )
+VALUES
+( 'Toy Story' ,1995,1,1),
+( 'Toy Story 2' ,1999,1,1),
+( 'Brigadoone' ,1954,2,1),
+( 'The Empire Strikes Back' ,1977,3,2),
+( 'Coda' ,2021,4,3),
+( 'Dune' ,2021,5,3);
 
 -- ------------------------------------------------------------------------------------
 -- 4. Insert into the actor table - Parent Table
-
+INSERT INTO actor
 
 -- ------------------------------------------------------------------------------------
 -- 5. Insert into the feature table - Parent Table
@@ -51,6 +68,12 @@ USE film;
 
 -- ------------------------------------------------------------------------------------
 -- 12. Insert into the movie_media table - Linking Table
-
+INSERT INTO movie_media
+( movie_id
+, media_id
+, price_id )
+VALUES
+( 1, 1, 1), -- Toy Story, DVD, 19.95
+( 5, 3, NULL)
 
 -- ------------------------------------------------------------------------------------
